7bd8b3a2e3d7ec60e7a323b60cb0da43802b435c
mpierrejean <morgane.pierrejean@gmail.com>
2017-01-13 12:22:38 +0100

    clean up plot function in simulations

--- inst/Simu_n=24000/R/02.evalMethods.R
+++ inst/Simu_n=24000/R/02.evalMethods.R
@@ -1,11 +1,13 @@
 library(c3co)
 library(reshape)
 library(tis)
+library(matrixStats)
 source("R/00.functions.R")
 framework <- "realistic"
 forceM <- FALSE
 stats <- c("TCN","C1C2", "TCN")
 meth <- c("FLLAT","c3co", "c3co")
+pathFig <- Arguments$getWritablePath("Figures")
 
 dataAnnotTP <- loadCnRegionData(dataSet="GSE13372", tumorFraction=1)
 dataAnnotN <- loadCnRegionData(dataSet="GSE13372", tumorFraction=0)
@@ -38,6 +40,7 @@ cMean <- mean(dataAnnotN$c)
 ###########################################
 ### Evaluation on weights
 ###########################################
+## Loss
 weightsArray <- array(dim=c(100, length(stats)),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats)))
 for(b in 1:100){
   print(b)
@@ -65,8 +68,40 @@ for(b in 1:100){
 dataArrayWeights <- data.frame(melt(weightsArray))
 gWeights <- ggplot(dataArrayWeights)+geom_boxplot(aes(x=method, y=value,fill=method))+ylab("Loss")+xlab("")+theme_bw()
 gWeights
-pathFig <- Arguments$getWritablePath("Figures/Weights")
 ggsave(gWeights,filename=sprintf("%s/weightLoss_n=24000.pdf",pathFig, framework), width=7, height=5)
+
+
+
+### RandIndex
+library(mclust)
+randIndexArray <- array(dim=c(100, length(stats)),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats)))
+pathWeight <- Arguments$getWritablePath(sprintf("weightData"))
+pathSim <- Arguments$getWritablePath(sprintf("simData"))
+for(b in 1:100){
+  print(b)
+  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
+  WT <- readRDS(file.path(pathWeight, filename))
+  
+  WT <- cbind(WT, 100-rowSums(WT))
+  clustWT <-  cutree(hclust(dist(WT),method="ward.D"), ncol(WT))
+  for(ss in 1:length(stats)){
+    stat=stats[ss]
+    mm <- meth[ss]
+    dataBest <- loadDataBest(mm, stat, framework, b)
+    W <- dataBest$W
+    clustW <- cutree(hclust(dist(WT),method="ward.D"), ncol(W))
+    randIndex <- adjustedRandIndex(clustWT, clustW)
+    randIndexArray[b,ss] <- randIndex
+  }
+}
+randIndexArrayFull <- cbind(randIndexArray)
+names(randIndexArrayFull) <- c("b", "method")
+dataArrayrandIndex <- data.frame(melt(randIndexArrayFull))
+dataArrayrandIndex$method <- factor(dataArrayrandIndex$X2, levels=c("FLLAT-TCN", "c3co-TCN", "c3co-C1C2"))
+gRandIndex <- ggplot(dataArrayrandIndex)+geom_boxplot(aes(x=method, y=value,fill=method))+ylab("Rand Index")+xlab("")+theme_bw()
+gRandIndex
+ggsave(gRandIndex,filename=sprintf("%s/RandIndex_n=24000.pdf",pathFig, framework), width=7, height=5)
+
 
 #######################################################################
 ### Archetypes ROC curves Full resolution
@@ -162,213 +197,10 @@ AUCs_arch<- apply(rocArrayArchFull, 2, function(roc){
 })
 df.auc.arch <- melt(AUCs_arch)
 df.auc.arch$AUC <- df.auc.arch$value
-ggplot(df.auc.arch)+geom_boxplot(aes(x=method, y=AUC, color=method))
-
-
-#######################################################################
-### Estimated Profiles ROC curves full res
-#######################################################################
-
-
-
-tol <- seq(from=0, to=1, length=20)
-tol <- sort(tol, decreasing=TRUE)
-
-
-rocDataPath <- Arguments$getWritablePath("rocDatac3co")
-fileROC <- "rocArray,full,profiles.rds"
-forceROC=FALSE
-if(!file.exists(file.path(rocDataPath, fileROC))||forceROC){
-  rocArrayProfFull <- array(dim=c(100, length(stats), 2,length(tol)),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats), ROC=c("tp", "fp")))
-  for(b in 1:100){
-    print(b)
-    for(ss in 1:length(stats)){
-      stat <- stats[ss]
-      mm <- meth[ss]      
-      filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-      M <- readRDS(file.path(pathWeight, filename))
-      newM <- t(apply(M, 1, function(mm) {
-        ww <- which(mm<0)
-        mm[ww] <- 0
-        return(mm)
-      }))
-      newM <- cbind(newM, 100-rowSums(newM))
-      
-      composition <- apply(newM, 1, function(mm) {
-        ww <- which(mm!=0)
-        return(ww)
-      })
-      
-      dataBest <- loadDataBest(mm, stat, framework, b)
-      
-###### altered loci determination
-      alteredLoci <- sapply(composition, function(cc){
-        alt <- rowSums(sapply(cc, function(c){
-          if(c!=length(bkpsByClones)){
-            res <- subClones[[c]]$region!="(1,1)"
-          }else{
-            res <- rep(FALSE, len) 
-          }
-          return(res)
-        }))        
-      })
-      alteredLociY <- apply(alteredLoci, 2, function(cc){
-        wwA <- which(cc!=0)
-        ww <- which(cc==0)
-        alt <- logical(0)
-        alt[wwA] <- TRUE
-        alt[ww] <- FALSE
-        return(alt)
-      })
-      if(stat=="C1C2"){
-        Y1hat <- dataBest$Y.hat$Y1
-        Y2hat <- dataBest$Y.hat$Y2
-        bkp <- dataBest$bkp
-        start <- c(1,ceiling(bkp))
-        end <- c(floor(bkp), len)
-        Y1hatFull <- expand(t(Y1hat),start, end)
-        Y2hatFull <- expand(t(Y2hat),start, end)
-        YhatFull <- expand(Y1hat+Y2hat,start, end)
-        SESP <- SESPC1C2(Y1hatFull,Y2hatFull,alteredLociY,ind=1:ncol(alteredLociY), tol, 1, 1)
-              
-      }else{
-        if(mm=="c3co"){
-          YTCNhat <- dataBest$Y.hat$Y1
-          bkp <- dataBest$bkp
-          start <- c(1,ceiling(bkp))
-          end <- c(floor(bkp), len)
-          YTCNhatFull <- expand(t(YTCNhat),start, end)
-        }else{
-          YTCNhatFull <- 2*2^(dataBest$Y.hat)
-        }
-        SESP <- SESPTCN(YTCNhatFull,alteredLociY,ind=1:ncol(alteredLociY), tol, 2)
-      }     
-      rocArrayProfFull[b,ss,"tp",] <- unlist(SESP["tp",])
-      rocArrayProfFull[b,ss,"fp",] <- unlist(SESP["fp",])
-    }
-  }
-  saveRDS(rocArrayProfFull, file.path(rocDataPath, fileROC))
-}else{
-  rocDataPath <- Arguments$getWritablePath("rocDatac3co")
-  fileROC <- "rocArray,full,profiles.rds"
-  rocArrayProfFull <- readRDS(file.path(rocDataPath, fileROC))
-}
-
-FPRs <-  sort(unique(as.vector(rocArrayProfFull[,,"fp",])))
-sumROCarrayProfFull <- apply(rocArrayProfFull, 2, function(roc) rowMeans(apply(roc,1,ComputeROC, FPRs), na.rm=TRUE))
-
-sumROCarrayProfFull <- apply(rocArrayProfFull, 2, function(roc) rowMeans(apply(roc,1,ComputeROC, FPRs), na.rm=TRUE))
-
-rownames(sumROCarrayProfFull) <- FPRs
-dataROCprofFull <- melt(sumROCarrayProfFull)
-colnames(dataROCprofFull) <- c("FPR", "method", "TPR")
-
-gplotROCprofFull <- ggplot(dataROCprofFull)+ geom_line(aes(x=FPR,y=TPR, group=method,colour = method, lty=method))+xlim(c(0,1))+geom_abline(slope=1, intercept=0, colour="red", lty=2)+ylim(c(0,1))+theme_bw()
-gplotROCprofFull+geom_point(aes(x=FPR,y=TPR, group=method,colour = method, lty=method))
-
-ggsave(gplotROCprofFull, filename=file.path(pathFig, "ROC,profiles,FullRes_n=2400.pdf"), width=7, height=5)
-
-
-AUCs <- apply(rocArrayProfFull, 2, function(roc){
-  apply(roc,1,ComputeAUC, FPRs)
-})
-
-df.auc <- melt(AUCs)
-df.auc$AUC <- df.auc$value
-ggplot(df.auc)+geom_boxplot(aes(x=method, y=AUC, color=method))
-
-
-####################################
-## Evaluation on Y observed
-####################################
-rocArrayYFull <- array(dim=c(100, 2, 2,length(tol)),dimnames=list(b=1:100, method=sprintf("%s",stats[2:3]), ROC=c("tp", "fp")))
-pathWeight <- Arguments$getWritablePath(sprintf("weightData"))
-pathSim <- Arguments$getWritablePath(sprintf("simData"))
-for(b in 1:100){
-  print(b)
-  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-  M <- readRDS(file.path(pathWeight, filename))
-  newM <- t(apply(M, 1, function(mm) {
-    ww <- which(mm<10)
-    mm[ww] <- 0
-    return(mm)
-  }))
-  newM <- cbind(newM, 100-rowSums(newM))
-  
-  composition <- apply(newM, 1, function(mm) {
-    ww <- which(mm!=0)
-    return(ww)
-  })
-  alteredLoci<- sapply(composition, function(cc){
-    alt <- rowSums(sapply(cc, function(c){
-      if(c!=length(bkpsByClones)){
-        res <- subClones[[c]]$region!="(1,1)"
-      }else{
-        res <- rep(FALSE, len) 
-      }
-      return(res)
-    }))
-    
-  })
-   alteredLociY <- apply(alteredLoci, 2, function(cc){
-        wwA <- which(cc!=0)
-        ww <- which(cc==0)
-        alt <- logical(0)
-        alt[wwA] <- TRUE
-        alt[ww] <- FALSE
-        return(alt)
-      })
-  for(ss in 2:3){
-    stat <- stats[ss]
-    pathArch <- Arguments$getWritablePath(sprintf("archetypeData%s_%s_%s", stat, framework, meth[ss]))
-    pathMeth <- Arguments$getWritablePath(sprintf("%s/features_B=%s/",pathArch,b))
-    
-    resSegmentation <-  readRDS(sprintf("%s/segDat.rds",pathMeth))
-    Y <- resSegmentation$Y
-    Y1 <- resSegmentation$Y1
-    Y2 <- resSegmentation$Y2
-    bkp <- resSegmentation$bkp[[1]]
-    start <- c(1,ceiling(bkp))
-    end <- c(floor(bkp), len)
-    if(stat=="C1C2"){
-      start <- c(1,ceiling(bkp))
-      end <- c(floor(bkp), len)
-      Y1hatFull <- expand(Y1,start, end)
-      Y2hatFull <- expand(Y2,start, end)
-      YhatFull <- expand(Y1+Y2,start, end)
-      SESP <- SESPC1C2(Y1hatFull,Y2hatFull,alteredLociY,ind=1:ncol(alteredLociY), tol, c1Mean, c2Mean)
-      
-    }else{
-      start <- c(1,ceiling(bkp))
-      end <- c(floor(bkp), len)
-      YTCNhatFull <- expand(Y,start, end)
-      SESP <- SESPTCN(YTCNhatFull,alteredLociY,ind=1:ncol(alteredLociY), tol, cMean)
-    }  
-    rocArrayYFull[b,stat,"tp",] <- unlist(SESP["tp",])
-    rocArrayYFull[b,stat,"fp",] <- unlist(SESP["fp",])   
-  }
-}
-
-
-AUCsY <- apply(rocArrayYFull, 2, function(roc){
-  apply(roc,1,ComputeAUC, FPRs)
-})
-
-df.aucY <- melt(AUCsY)
-df.aucY$AUC <- df.aucY$value
-
-df.aucY <- melt(colMeans(AUCsY))
-df.aucY$Y <- factor(rownames(df.aucY))
-
-df.auc.tot <- rbind(cbind(df.auc.arch, stat="Z") , cbind(df.auc, stat="Yhat"))
-
-
-gAUC <- ggplot(df.auc.tot, aes(x=method, y=AUC))+geom_boxplot(aes( fill=method))+theme_bw()+geom_hline(data=df.aucY, aes(yintercept=value, lty=Y),show_guide = TRUE)+facet_grid(.~stat)
+df.auc.arch$method <- factor(df.auc.arch$method, levels=c("FLLAT-TCN","c3co-TCN","c3co-C1C2"))
+gAUC <- ggplot(df.auc.arch, aes(x=method, y=AUC))+geom_boxplot(aes( fill=method))+theme_bw()+xlab("")
 gAUC
-pathFig <- Arguments$getWritablePath("Figures/AUC")
-ggsave(gAUC, filename=file.path(pathFig, "AUCs_n=24000.pdf"), width=10, height=7)
-
-
+ggsave(gAUC, filename=file.path(pathFig, "AUCs_n=24000,Z.pdf"), width=10, height=5)
 
 ### PVE
 PVEArray <- array(dim=c(100, length(stats), 15),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats)))
@@ -414,242 +246,18 @@ for(b in 1:100){
 meanPVE <- na.omit(apply(PVEArray, 2,colMeans, na.rm=TRUE))
 sdPVE <- na.omit(apply(PVEArray, 2,colSds, na.rm=TRUE))
 
+## data to plot PVE
 dataPVE <- cbind(melt(meanPVE), melt(sdPVE))[,-c(4,5)]
 colnames(dataPVE) <- c("Features", "method", "PVE", "sds")
 dataPVE$Features <- dataPVE$Features+1
+dataPVE$method <- factor(dataPVE$method, , levels=c("FLLAT-TCN","c3co-TCN","c3co-C1C2"))
 pPVE <-  ggplot(dataPVE)+geom_line(aes(x=Features,y=PVE,colour=method, lty=method))
 
 pPVE <- pPVE+geom_ribbon(aes(ymin=PVE-sds, ymax=PVE+sds,x=Features,fill=method ),alpha=0.3)+ scale_x_continuous(limits=c(2, 15), breaks=seq(from=2, to =15, by=1))+theme_bw()+scale_y_continuous(limits=c(0.15,1))
-pathFig <- Arguments$getWritablePath("Figures/PVE")
+pPVE
 ggsave(pPVE, filename=file.path(pathFig, "PVEs_n=24000.pdf"), width=7, height=5)
 
-### Weights comparisons
-library(mclust)
-randIndexArray <- array(dim=c(100, length(stats)),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats)))
-for(b in 1:100){
-  print(b)
-  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-  WT <- readRDS(file.path(pathWeight, filename))
- 
-  WT <- cbind(WT, 100-rowSums(WT))
-  clustWT <-  cutree(hclust(dist(WT),method="ward.D"), ncol(WT))
-  for(ss in 1:length(stats)){
-    stat=stats[ss]
-    mm <- meth[ss]
-    dataBest <- loadDataBest(mm, stat, framework, b)
-    W <- dataBest$W
-    clustW <- cutree(hclust(dist(WT),method="ward.D"), ncol(W))
-    randIndex <- adjustedRandIndex(clustWT, clustW)
-    randIndexArray[b,ss] <- randIndex
-  }
-}
-
-
-
-randIndexArrayY <- array(dim=c(100, 1),dimnames=list(b=1:100, method="Y"))
-pathWeight <- Arguments$getWritablePath(sprintf("weightData"))
-pathSim <- Arguments$getWritablePath(sprintf("simData"))
-for(b in 1:100){
-  print(b)
-  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-  WT <- readRDS(file.path(pathWeight, filename))
- 
-  WT <- cbind(WT, 100-rowSums(WT))
-  clustWT <-  cutree(hclust(dist(WT),method="ward.D"), ncol(WT))
-  
-  dat <- readRDS(sprintf("%s/dat_B=%s.rds",pathSim,b))
-  tcnB <- t(sapply(dat, function(dd) dd$tcn))
-  clustY <- cutree(hclust(dist(tcnB),method="ward.D"), ncol(WT))
-  randIndex <- adjustedRandIndex(clustWT, clustW)
-  randIndexArrayY[b] <-  adjustedRandIndex(clustWT, clustY)
-}
-
-dataArrayrandIndex <- data.frame(melt(randIndexArray))
-gRandIndex <- ggplot(dataArrayrandIndex)+geom_boxplot(aes(x=method, y=value,fill=method))+ylab("Rand Index")+xlab("")+theme_bw()+geom_hline(yintercept=mean(randIndexArrayY),show_guide = TRUE)
-gRandIndex
-
-pathFig <- Arguments$getWritablePath("Figures/Weights")
-ggsave(gRandIndex,filename=sprintf("%s/RandIndex_n=24000.pdf",pathFig, framework), width=7, height=5)
 
 
 
 
-#######################################################################
-### Binary evaluation on Yhat
-#######################################################################
-
-tol <- c(seq(from=0.0, to=1, length=20))
-tol <- sort(tol, decreasing=TRUE)
-
-alteredLociInClones <- sapply(1:(length(regionsByClones)-1), function(c){
-  if(c!=length(regionsByClones)){
-    res <- subClones[[c]]$region!="(1,1)"
-  }else{
-    res <- rep(FALSE, len) 
-  }
-  return(res)
-})
-
-
-
-if(!file.exists(file.path(rocDataPath, fileROC))||forceROC){
-  for(b in 1:100){
-    print(b)
-    filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-    M <- readRDS(file.path(pathWeight, filename))
-    WT <- cbind(M, 100-rowSums(M))/100
-    composition <- apply(WT, 1, function(mm) {
-      ww <- which(mm!=0)
-      return(ww)
-    })
-
-    alteredLoci<- sapply(composition, function(cc){
-        alt <- rowSums(sapply(cc, function(c){
-          if(c!=length(bkpsByClones)){
-            res <- subClones[[c]]$region!="(1,1)"
-          }else{
-            res <- rep(FALSE, len) 
-          }
-          return(res)
-        }))
-        
-      })
-      alteredLociY <- apply(alteredLoci, 2, function(cc){
-        wwA <- which(cc!=0)
-        ww <- which(cc==0)
-        alt <- logical(0)
-        alt[wwA] <- TRUE
-        alt[ww] <- FALSE
-        return(alt)
-      })
-    
-    for(ss in 1:length(stats)){
-      stat=stats[ss]
-      mm <- meth[ss]      
-      dataBest <- loadDataBest(mm,stat, framework, b)
-      Z <- dataBest$Z
-      W <- dataBest$W
-      eps <- 0.1
-      corrBestW <- apply(WT,2,function(zz){
-        apply(W,2,function(zzh){
-          sum(abs(zz-zzh)<eps,na.rm=T)/length(zzh)
-        }) 
-      })
-      ind <- apply(corrBestW, 2, which.max)
-      compositionWhat <- apply(W, 1, function(mm) {
-        ww <- which(mm!=0)
-        return(ww)
-      })
-      
-      
-      if(stat=="C1C2"){
-        Z1 <- dataBest$Z1
-        Z2 <- dataBest$Z2
-        bkp <- dataBest$bkp
-        start <- c(1,ceiling(bkp))
-        end <- c(floor(bkp), len)
-        Z1hatFull <- expand(Z1,start, end)
-        Z2hatFull <- expand(Z2,start, end)
-        
-        alteredLociZ1Z2 <- sapply(1:nrow(Z1hatFull), function(ii) {
-          getPosC1C2( tt=tt, c1Mean=1,c2Mean=1, zz1=Z1hatFull[ii,],zz2=Z2hatFull[ii,])
-        }   )
-
-        alteredLociYhat <- sapply(compositionWhat, function(cc) rowSums(alteredLociZ1Z2[,cc]))
-        alteredLociYhat <- apply(alteredLociYhat, 2, function(cc){
-          wwA <- which(cc!=0)
-          ww <- which(cc==0)
-          alt <- logical(0)
-          alt[wwA] <- TRUE
-          alt[ww] <- FALSE
-          return(alt)
-        })
-
-        tpfp <- sapply(1:nrow(alteredLociYhat), function(llhat){
-          sapply(1:nrow(alteredLociY), function(llobs){
-            tp <- sum(llobs==llhat)
-            pos <- sum(llobs)
-            fp <- length(which(llobs==FALSE & llhat==TRUE))
-            neg <- length(llobs)-sum(llobs)
-            return(list(tp=tp, fp=fp, neg=neg, pos=pos))
-          })       
-        })
-        
-      }else{
-        if(mm=="FLLAT"){
-          ZhatFull <- t(2*2^Z)          
-        }else{
-          bkp <- dataBest$bkp
-          start <- c(1,ceiling(bkp))
-          end <- c(floor(bkp), len)            
-          ZhatFull <- expand(Z,start, end)
-        }
-        SESP <- SESPTCN(ZhatFull,alteredLociInClones, ind,tol, 2)
-      
-      }
-      SESP.mat <- rbind(tp=unlist(SESP["tp",]),fp=unlist(SESP["fp",]))
-      AUCs_arch[b,ss] <- ComputeAUC(SESP.mat,unlist(SESP["fp",]))
-      rocArrayArchFull[b,ss,"tp",] <- unlist(SESP["tp",])
-      rocArrayArchFull[b,ss,"fp",] <- unlist(SESP["fp",])
-      
-    }
-  }
-  saveRDS(rocArrayArchFull, file.path(rocDataPath, fileROC))
-}else{
-  rocArrayArchFull <- readRDS(file.path(rocDataPath, fileROC))
-}
-
-
-
-
-
-### Weights comparisons
-library(mclust)
-randIndexArray <- array(dim=c(100, length(stats)),dimnames=list(b=1:100, method=sprintf("%s-%s",meth,stats)))
-pathWeight <- Arguments$getWritablePath(sprintf("weightData"))
-pathSim <- Arguments$getWritablePath(sprintf("simData"))
-for(b in 1:100){
-  print(b)
-  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-  WT <- readRDS(file.path(pathWeight, filename))
- 
-  WT <- cbind(WT, 100-rowSums(WT))
-  clustWT <-  cutree(hclust(dist(WT),method="ward.D"), ncol(WT))
-  for(ss in 1:length(stats)){
-    stat=stats[ss]
-    mm <- meth[ss]
-    dataBest <- loadDataBest(mm, stat, framework, b)
-    W <- dataBest$W
-    clustW <- cutree(hclust(dist(WT),method="ward.D"), ncol(W))
-    randIndex <- adjustedRandIndex(clustWT, clustW)
-    randIndexArray[b,ss] <- randIndex
-  }
-}
-
-randIndexArrayY <- array(dim=c(100, 1),dimnames=list(b=1:100, method="Y"))
-
-for(b in 1:100){
-  print(b)
-  filename <- sprintf("weight,n=%s,b=%s.rds", n,b)
-  WT <- readRDS(file.path(pathWeight, filename))
- 
-  WT <- cbind(WT, 100-rowSums(WT))
-  clustWT <-  cutree(hclust(dist(WT),method="ward.D"), ncol(WT))
-  
-  dat <- readRDS(sprintf("%s/dat_B=%s.rds",pathSim,b))
-  tcnB <- t(sapply(dat, function(dd) dd$tcn))
-  clustY <- cutree(hclust(dist(tcnB),method="ward.D"), ncol(WT))
-  randIndex <- adjustedRandIndex(clustWT, clustW)
-  randIndexArrayY[b] <-  adjustedRandIndex(clustWT, clustY)
-}
-
-
-randIndexArrayFull <- cbind(randIndexArray, randIndexArrayY)
-names(randIndexArrayFull) <- c("b", "method")
-dataArrayrandIndex <- data.frame(melt(randIndexArrayFull))
-dataArrayrandIndex$method <- dataArrayrandIndex$X2
-gRandIndex <- ggplot(dataArrayrandIndex)+geom_boxplot(aes(x=method, y=value,fill=method))+ylab("Rand Index")+xlab("")+theme_bw()
-gRandIndex
-
-pathFig <- Arguments$getWritablePath("Figures/Weights")
-ggsave(gRandIndex,filename=sprintf("%s/RandIndex_n=24000.pdf",pathFig, framework), width=7, height=5)
