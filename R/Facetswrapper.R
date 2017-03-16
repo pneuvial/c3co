46666eb114ee40085d093a273938ee6dc5204c33
Pierre Neuvial <pneuvial@gmail.com>
2017-03-16 11:12:34 +0100

    Tentative fix for Travis CI error due to the fact that the line     get(gbuild, pos = "package:pctGCdata") in the facets package fails if the package 'pctGCdata' is not loaded

--- R/Facetswrapper.R
+++ R/Facetswrapper.R
@@ -27,7 +27,7 @@ loadFacetsdata <- function(pathFacets){
 #' @param stat "TCN or "C1C2" paramater to segment the data. If \code{stat==TCN}, the segmentation will be done on TCN only. 
 #' @return A list which contains the breakpoints by chromosome and also the binning of TCN, C1 and C2.
 #' @examples
-#' if (requireNamespace("facets", quietly=TRUE) & requireNamespace("pctGCdata", quietly=TRUE)) {
+#' if (require("facets", quietly=TRUE)) {
 #' pathFacets <- system.file("extdata",package="facets")
 #' output.dir <- R.utils::Arguments$getWritablePath("output")
 #' Facetswrapper(pathFacets,output.dir=output.dir, stat="TCN")
