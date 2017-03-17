c270d4f2c2e302084eef5582320dc2c89021a58d
Pierre Neuvial <pneuvial@gmail.com>
2017-03-17 01:10:23 +0100

    minor edits

--- inst/GSE47077/R/03.runC3co.R
+++ inst/GSE47077/R/03.runC3co.R
@@ -2,12 +2,14 @@
 # Run c3co
 ##########################################################################
 ## Run 00.preprocessing.R, 01.CallGenotypes.R, 03.TumorBoost.R before running this file
-library(c3co)
+library("c3co")
 patient <- "RK29"
 path <- "data"
 dat <- readRDS(sprintf("%s/dat-%s.rds", path ,patient))
-output.dir <- R.utils::Arguments$getWritablePath(sprintf("results_c3co-%s",patient))
+output.dir <- sprintf("results_c3co-%s",patient)
 
-resC3co <- c3co(dat,lambda1.grid=c(2e-6,1e-5,2e-5),lambda2.grid=c(2e-6,1e-5,2e-5), saveResults=TRUE, output.dir=output.dir,nb.arch.grid = 2:9)
+grid <- c(2e-6,1e-5,2e-5)
+resC3co <- c3co(dat,lambda1.grid=grid, lambda2.grid=grid, 
+                saveResults=TRUE, output.dir=output.dir,nb.arch.grid = 2:9)
 
 
