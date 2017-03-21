ab15c2660a50eb28674339f73c4d1e92001101b3
Pierre Neuvial <pneuvial@gmail.com>
2017-03-21 18:00:02 +0100

    cleanups in GSE47077 figure scripts

--- inst/GSE47077/R/04.figures.R
+++ inst/GSE47077/R/04.figures.R
@@ -5,18 +5,21 @@
 
 library("c3co")
 library("RColorBrewer")
+library("ggplot2")
 
 patient <- "RK29"
 
 ### Plot PVE
 pvePlot(resC3co@fit, ylim=c(0.70,1))
-dataBest <- resC3co@fit[[6]]
+bestp <- 6
+dataBest <- resC3co@fit[[bestp]]
 
 ### Plot W matrix
 figPath <- R.utils::Arguments$getWritablePath("fig-GSE47077-RK29")
-
-pdf(file.path(figPath,sprintf("heatmap,GSE47077,patient=%s.pdf",patient)), width=13, height=8)
-Wplot(resC3co, idxBest=6, rownamesW= sprintf("R%s",1:nrow(dataBest@W)))
+filename <- sprintf("heatmap,GSE47077,patient=%s.pdf", patient)
+pathname <- file.path(figPath, filename)
+pdf(pathname, width=13, height=8)
+Wplot(resC3co, idxBest=bestp, rownamesW=sprintf("R%s",1:nrow(dataBest@W)))
 dev.off()
 
 ### Plot Latent profiles
@@ -24,44 +27,22 @@ pathCHR <-  system.file("inst", "GSE47077", "data","minMaxposByCHR.rds", package
 minMaxPos <- readRDS(pathCHR)
 
 lengthCHR <- sapply(resC3co@bkp, length)
-chrs <- sapply(1:22, function(cc) rep(cc,times=lengthCHR[cc]))
+chrs <- sapply(1:22, function(cc) rep(cc, times=lengthCHR[cc]))
 
-start <- c(1,cumsum(lengthCHR)+1)
+start <- c(1, cumsum(lengthCHR) + 1)
 
-ch <- c(9)
-df.CHR <- createZdf(resC3co, minMaxPos, chromosomes=ch, var="TCN", idxBest=6)
-
-df.CHRC1 <- createZdf(resC3co, minMaxPos, chromosomes=ch, var="Minor", idxBest=6)
-df.CHRC2 <- createZdf(resC3co, minMaxPos, chromosomes=ch, var="Major", idxBest=6)
-
-df.CHR$position <- df.CHR$position/1e6
-df.CHRC2$position <- df.CHRC2$position/1e6
-df.CHRC1$position <- df.CHRC1$position/1e6
-
-gArchTCN <- Zplot(df.CHR, ylab="TCN", ylim=c(1,3))
-gArchC2 <- Zplot(df.CHRC2, ylab="Major", ylim=c(1,3))
-gArchC1 <- Zplot(df.CHRC1, ylab="Minor", ylim=c(0,2))
-
-ggsave(gArchTCN, filename=sprintf("%s/archetypes,GSE47077,patient=%s,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)
-ggsave(gArchC1, filename=sprintf("%s/archetypes,GSE47077,patient=%s,C1,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)                    
-ggsave(gArchC2, filename=sprintf("%s/archetypes,GSE47077,patient=%s,C2,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)
-
-
-
-ch <- c(14)
-df.CHR <- createZdf( resC3co,minMaxPos, chromosomes=ch, var="TCN", idxBest=6)
-
-df.CHRC1 <- createZdf( resC3co, minMaxPos, chromosomes=ch, var="Minor", idxBest=6)
-df.CHRC2 <- createZdf( resC3co, minMaxPos, chromosomes=ch, var="Major", idxBest=6)
-
-df.CHR$position <- df.CHR$position/1e6
-df.CHRC2$position <- df.CHRC2$position/1e6
-df.CHRC1$position <- df.CHRC1$position/1e6
-
-gArchTCN <- Zplot(df.CHR, ylab="TCN")
-gArchC2 <- Zplot(df.CHRC2, ylab="Major")
-gArchC1 <- Zplot(df.CHRC1, ylab="Minor")
-ggsave(gArchTCN, filename=sprintf("%s/archetypes,GSE47077,patient=%s,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)
-ggsave(gArchC1, filename=sprintf("%s/archetypes,GSE47077,patient=%s,C1,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)                    
-ggsave(gArchC2, filename=sprintf("%s/archetypes,GSE47077,patient=%s,C2,chr=%s.pdf", figPath, patient,ch), width=7, height=3.5)
-
+stats <- c("TCN", "Minor", "Major")
+ylims <- list("TCN"=c(1, 3), 
+              "Minor"=c(0, 2), 
+              "Major"= c(1, 3))
+for (ch in c(9, 14)) {
+    for (what in stats) {
+        df <- createZdf(resC3co, minMaxPos, chromosomes=ch, var=what, idxBest=bestp)
+        df$position <- df$position/1e6 ## scale to Mb
+        
+        filename <- sprintf("archetypes,GSE47077,patient=%s,%s,chr=%s.pdf", patient, what, ch)
+        pathname <- file.path(figPath, filename)
+        gArch <- Zplot(df, ylab=what)
+        ggsave(gArch, filename=pathname, width=7, height=3.5)
+    }
+}
