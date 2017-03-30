df4df27c700942407c37bb0afd786a533062ad28
Pierre Neuvial <pneuvial@gmail.com>
2017-03-30 16:01:31 -0600

    more dense grids for GSE47077

--- inst/GSE47077/R/03.runC3co.R
+++ inst/GSE47077/R/03.runC3co.R
@@ -3,11 +3,16 @@
 ##########################################################################
 ## Run 00.preprocessing.R, 01.CallGenotypes.R, 03.TumorBoost.R before running this file
 library("c3co")
-patient <- "RK29"
-path <- "data"
-dat <- readRDS(sprintf("%s/dat-%s.rds", path ,patient))
+path <- Arguments$getWritablePath("c3co-data")
+filename <- sprintf("dat-%s.rds", patientID)
+pathname <- file.path(path, filename)
+datList <- readRDS(pathname)
 
-lambda.grid <- c(2e-6,1e-5,2e-5) ## penalty
-p.list <- 2:9 ## candidate number of subclones
+lambda.grid <- seq(from=1e-6, to=1e-4, length=10)
+p.list <- 2:length(datList) ## candidate number of subclones
 parameters.grid <- list(lambda1=lambda.grid, lambda2=lambda.grid, nb.arch = p.list)
-resC3co <- c3co(dat, parameters.grid = parameters.grid, verbose=TRUE)
+
+resC3co <- c3co(datList, pathSeg="segDat.rds", parameters.grid = parameters.grid, verbose=TRUE)
+#seg <- resC3co@segDat
+#seg$bkp <- resC3co@bkp
+#saveRDS(seg, "segDat.rds")
