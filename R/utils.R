cbcc60505375215645f3a1baa7c9663a9c71aa68
hb <hb@aroma-project.org>
2018-02-27 14:17:49 -0800

    Using message() instead of print()

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,10 @@ mstr <- function(...) {
   message(paste(capture.output(str(...)), collapse = "\n"))
 }
 
+mprint <- function(...) {
+  message(paste(capture.output(print(...)), collapse = "\n"))
+}
+
 mprintf <- function(...) {
   message(sprintf(...))
 }
--- tests/testthat/test_PSCBS.R
+++ tests/testthat/test_PSCBS.R
@@ -9,11 +9,11 @@ if (requireNamespace("c3co.data")) {
               all(c("Y1", "Y2", "Y", "bkp") %in% names(segDat)))
   })
   
-  set.seed(7)
-  lambda.grid <- seq(from = 1e-4, to = 1e-3, length.out = 10)
-  parameters.grid <- list(lambda = lambda.grid, nb.arch = 2:5)
-
   test_that("c3co terminates on PSCBS locus-level data", {
+    set.seed(7)
+    lambda.grid <- seq(from = 1e-4, to = 1e-3, length.out = 10)
+    parameters.grid <- list(lambda = lambda.grid, nb.arch = 2:5)
+    segDat <- PSCBSwrapper(PSCBSdata, stat = "C1C2")
     res <- c3co(dat = NULL, parameters.grid, segDat = segDat)
   })
 }
