d273d7dd48ca598073e50ec1a661318a50fefac0
Pierre Neuvial <pneuvial@gmail.com>
2019-02-19 09:42:36 +0100

    more reshaping in getToyData returning locus level data is now optional

--- R/getToyData.R
+++ R/getToyData.R
@@ -17,15 +17,22 @@
 #' @param intercept A logical value indicating whether an intercept should be
 #'   added (this corresponds to the presence of a normal subclone).
 #' 
-#' @return A list with three elements: `W`, a `n`-by-`nbClones`
-#'   matrix of weights, and `locus` and `segment`, which contain
-#'   locus-level and segment-level data. Each of them is a list of two
-#'   elements:
+#' @return A list with the following elements: 
+#'  \describe{
+#'  \item{W}{A `n`-by-`nbClones` matrix of weights}
+#'  \item{segment}{A list of two elements:
 #'   \describe{
-#'     \item{Y}{An `n`-by-`len` (or `nbBkp+1`) matrix of observed CN signals}
-#'     \item{Z}{An `nbClones`-by-`len` (or `nbBkp+1`) matrix of latent features
-#'              (subclones)}  
-#'   }
+#'     \item{Y}{An `n`-by-`nbBkp+1` matrix of observed CN signals}
+#'     \item{Z}{An `nbClones`-by-`nbBkp+1` matrix of latent features
+#'              (subclones)}}
+#'  }
+#'  \item{locus}{only returned if \code{returnLocus} is TRUE: A list of two elements:
+#'   \describe{
+#'     \item{Y}{An `n`-by-`len` matrix of observed CN signals}
+#'     \item{Z}{An `nbClones`-by-`len` matrix of latent features
+#'              (subclones)}}
+#'  }
+#'  }
 #'   
 #' @details For simplicity, the breakpoints positions are drawn uniformly from 
 #'   the set of all possible positions.
@@ -67,33 +74,35 @@
 #' fit <- fitC3co(Y, parameters.grid=parameters.grid)
 #' pvePlot2(fit$config$best)
 #' 
-getToyData <- function(n, len, nbClones, nbBkps, eps, weightSparsity = 0.1, intercept = TRUE) {
+getToyData <- function(n, len, nbClones, nbBkps, eps, weightSparsity = 0.1, intercept = TRUE, returnLocus = TRUE) {
     
     ## number of segments
     nbSegs <- nbBkps + 1 
     
     ## breakpoint positions
-    bkp <- sample(len, size=nbBkps, replace=FALSE)
+    bkp <- sample(len, size = nbBkps, replace = FALSE)
     bkp <- sort(bkp)
     
     ## segment lengths
     segLens <- diff(c(0, bkp))
-    segLens <- c(segLens, len-sum(segLens))
+    segLens <- c(segLens, len - sum(segLens))
     
+    if (intercept) {    
+        nbClones <- nbClones+1
+    }
     ## weights
     ru <- runif(n*nbClones)
     ru[ru < weightSparsity] <- 0
-    W <- matrix(ru, nrow=n, ncol=nbClones)
+    W <- matrix(ru, nrow = n, ncol = nbClones)
     W <- round(sweep(W, MARGIN = 1L, STATS = rowSums(W), FUN = `/`), digits = 2L)
-    W[, nbClones] <- 1-rowSums(W[, -nbClones, drop=FALSE]) ## make sure rows sum to 1 after rounding
+    W[, nbClones] <- 1 - rowSums(W[, -nbClones, drop = FALSE]) ## make sure rows sum to 1 after rounding
     
     Zlist <- list()
     ## subclones (segment-level)
     z <- rnorm(nbSegs*nbClones)
     Zs <- round(matrix(z, nrow = nbClones, ncol = nbSegs))
-    if (intercept) {  ## <=> adding a 'normal' clone
-        nbClones <- nbClones+1
-        Zs <- rbind(Zs, 1)
+    if (intercept) {  ## last clone should be 'normal'
+        Zs[nbClones, ] <- 1
     }
     
     ## subclones (locus-level)
@@ -105,11 +114,16 @@ getToyData <- function(n, len, nbClones, nbBkps, eps, weightSparsity = 0.1, inte
     Ys <- W %*% Zs + Es                   ## observations
     seg <- list(Y=Ys, Z=Zs)
     
+    res <- list(W = W, segment = seg)
+
     ## locus-level data
-    e <- rnorm(n*len, sd=eps)
-    El <- matrix(e, nrow=n, ncol=len)  ## noise
-    Yl <- W %*% Zl + El                ## observations
-    loc <- list(Y=Yl, Z=Zl)
-        
-    list(W = W, locus = loc, segment = seg)
+    if (returnLocus) {
+        e <- rnorm(n*len, sd = eps)
+        El <- matrix(e, nrow = n, ncol = len)  ## noise
+        Yl <- W %*% Zl + El                ## observations
+        loc <- list(Y = Yl, Z = Zl)
+        res$locus <- loc
+    }
+    
+    res
 }
--- tests/testthat/test_getToyData.R
+++ tests/testthat/test_getToyData.R
@@ -9,43 +9,33 @@ test_that("output of getToyData has correct dimensions and contents", {
     nbBkps <- 5
     eps <- 1
     n <- 4
-    dat <- getToyData(n, len, nbClones, nbBkps, eps=0, intercept=TRUE)  ## noiseless
     
-    ## weights
-    expect_equal(nrow(dat$W), n)
-    expect_equal(ncol(dat$W), nbClones+1)  ## +1 because of intercept
-
-    ## locus-level data
-    ldat <- dat$locus
-    expect_equal(nrow(ldat$Y), n)
-    expect_equal(ncol(ldat$Y), len)
-    expect_equal(nrow(ldat$Z), nbClones+1)
-    expect_equal(ncol(ldat$Z), len)
-    
-    ## segment-level data
-    sdat <- dat$segment
-    expect_equal(nrow(sdat$Y), n)
-    expect_equal(ncol(sdat$Y), nbBkps+1)
-    expect_equal(nrow(sdat$Z), nbClones+1)
-    expect_equal(ncol(sdat$Z), nbBkps+1)
-    
-    dat <- getToyData(n, len, nbClones, nbBkps, eps=0, intercept=FALSE)  ## noiseless
-    
-    ## weights
-    expect_equal(nrow(dat$W), n)
-    expect_equal(ncol(dat$W), nbClones)
-    
-    ## locus-level data
-    ldat <- dat$locus
-    expect_equal(nrow(ldat$Y), n)
-    expect_equal(ncol(ldat$Y), len)
-    expect_equal(nrow(ldat$Z), nbClones)
-    expect_equal(ncol(ldat$Z), len)
-    
-    ## segment-level data
-    sdat <- dat$segment
-    expect_equal(nrow(sdat$Y), n)
-    expect_equal(ncol(sdat$Y), nbBkps+1)
-    expect_equal(nrow(sdat$Z), nbClones)
-    expect_equal(ncol(sdat$Z), nbBkps+1)
+    for (intercept in c(TRUE, FALSE)) {
+        for (returnLocus in c(TRUE, FALSE)) {
+            dat <- getToyData(n, len, nbClones, nbBkps, eps=0, 
+                              intercept = intercept,
+                              returnLocus = returnLocus) 
+            
+            ## weights
+            expect_equal(nrow(dat$W), n)
+            expect_equal(ncol(dat$W), nbClones + intercept)
+            
+            ## locus-level data
+            ldat <- dat$locus
+            if (returnLocus) {
+                expect_equal(nrow(ldat$Y), n)
+                expect_equal(ncol(ldat$Y), len)
+                expect_equal(nrow(ldat$Z), nbClones + intercept)
+                expect_equal(ncol(ldat$Z), len)
+            } else {
+                expect_null(ldat)
+            }        
+            ## segment-level data
+            sdat <- dat$segment
+            expect_equal(nrow(sdat$Y), n)
+            expect_equal(ncol(sdat$Y), nbBkps+1)
+            expect_equal(nrow(sdat$Z), nbClones + intercept)
+            expect_equal(ncol(sdat$Z), nbBkps+1)
+        }    
+    }
 })
