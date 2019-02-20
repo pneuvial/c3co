4c3f630732af67fbe933227d105a62a1179b237e
Pierre Neuvial <pneuvial@gmail.com>
2019-02-20 10:43:13 +0100

    updated test for get.W to explicitly check for errors when dimensions are not as required

--- tests/testthat/test_get.W.R
+++ tests/testthat/test_get.W.R
@@ -11,27 +11,30 @@ test_that("get.W recovers W in noiseless situations", {
         nbSamples = c(2, 4, 6)  ## cannot test with nbSamples = 1 if nbClones cannot be set to 1!
     )
     
-    configs <- subset(configs, nbSamples >= nbClones)
-    configs <- subset(configs, nbSegs >= nbClones)
-    
     for (cc in 1:nrow(configs)) {
         config <- configs[cc, ]
         dime <- config[["sigDim"]]
-        dat <- getToyData(n = config[["nbSamples"]], len = config[["sigSize"]], 
-                          nbClones = config[["nbClones"]], nbSegs = config[["nbSegs"]], 
-                          dimension = dime, eps = 0)
-        W <- dat$W
-        Y <- dat$loc$Y
-        Z <- dat$loc$Z
-        
-        if (dime > 1) {
-            ## stack dimensions (if any)
-            Y <- do.call(cbind, args = Y)
-            Z <- do.call(cbind, args = Z)
-        }    
-        Zt <- t(Z)
-        What <- c3co:::get.W(Zt, Y)
-        expect_lt(max((W - What)^2), 1e-2)
+        if ((config[["nbSamples"]] < config[["nbClones"]]) || (config[["nbSegs"]] < config[["nbClones"]])) {
+            expect_error(getToyData(n = config[["nbSamples"]], len = config[["sigSize"]], 
+                                    nbClones = config[["nbClones"]], nbSegs = config[["nbSegs"]], 
+                                    dimension = dime, eps = 0))
+        } else {
+            dat <- getToyData(n = config[["nbSamples"]], len = config[["sigSize"]], 
+                              nbClones = config[["nbClones"]], nbSegs = config[["nbSegs"]], 
+                              dimension = dime, eps = 0)
+            W <- dat$W
+            Y <- dat$loc$Y
+            Z <- dat$loc$Z
+            
+            if (dime > 1) {
+                ## stack dimensions (if any)
+                Y <- do.call(cbind, args = Y)
+                Z <- do.call(cbind, args = Z)
+            }    
+            Zt <- t(Z)
+            What <- c3co:::get.W(Zt, Y)
+            expect_lt(max((W - What)^2), 1e-2)
+        }
     }
 })
 
