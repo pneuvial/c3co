e41945495fddb89788a2008254787f16035ae4a3
Pierre Neuvial <pneuvial@gmail.com>
2019-02-21 12:40:55 +0100

    improved test script: noiseless, easy settings

--- inst/testScripts/2019-02_Agro/fitC3co_noiseless.R
+++ inst/testScripts/2019-02_Agro/fitC3co_noiseless.R
@@ -1,29 +1,51 @@
-len <- 100
-n <- 10
+J <- 5 # segments
 
-z0 <- c(1, 1, 1, 1, 1)
-z1 <- c(1, 0, 1, 1, 1)
-z2 <- c(1, 1, 1, 0, 1)
+# subclones
+z0 <- rep(1L, K)
+z1 <- z0
+z1[2] <- 0
+z2 <- z0
+z2[4] <- 0
 Z <- rbind(z0, z1, z2)
 nbBkps <- ncol(Z)-1
 
+# weights
 W <- diag(rep(1, nrow(Z)))  ## identity weights
 Y <- W %*% Z                ## noiseless
-matplot(t(Y), t='s')
 
-params <- list(lambda=1e-5, nb.arch=2:3)
-res <- fitC3co(Y, parameters.grid=params)
+# non-penalized
+params <- list(lambda = 0, nb.arch = 1:3)
+res <- fitC3co(Y, parameters.grid = params)
+sapply(res$fit, modelFitStats) # model fit stats
+Zhats <- lapply(res$fit, function(fit) t(fit@Zt$Z))
 
-## 3 subclones (true value)
-fit2 <- res$fit[[2]]
+Zhats[[1]] - colMeans(Z)  ## close to 0
+Zhats[[2]]
+Zhats[[3]] - Z            ## close to 0
 
-modelFitStats(fit2)  ## pretty good
-round(fit2@W, 2)    ## W
+# penalized
+params <- list(lambda = 1e-2, nb.arch = 1:K)
+res <- fitC3co(Y, parameters.grid = params)
+sapply(res$fit, modelFitStats) # model fit stats
 
-round(fit2@Zt$Z, 2)  ## t(Z)!
-round(t(fit2@Zt$Z), 2) - Z ## 
+Zhats <- lapply(res$fit, function(fit) t(fit@Zt$Z))
 
-## 2 subclones (= number of non-normal subclones)
-fit1 <- res$fit[[1]]
-modelFitStats(fit1)  ## not so good! 
+Zhats[[1]] - colMeans(Z)  ## less close to 0
+Zhats[[2]]                
+Zhats[[3]] - Z            ## less close to 0
 
+# - - - - - - - - - - - - 
+# more samples
+# - - - - - - - - - - - - 
+n <- 10
+K <- nrow(Z)
+
+W <- rSparseWeightMatrix(nb.samp = n, nb.arch = K, sparse.coeff=0.5)
+Y <- W %*% Z                ## noiseless
+Y <- as.matrix(Y)
+
+# non-penalized
+params <- list(lambda = 0, nb.arch = 1:8)
+res <- fitC3co(Y, parameters.grid = params)
+pvePlot2(res$config$best)
+# ! negative PVE for a single sublclone. pb due to centering?
