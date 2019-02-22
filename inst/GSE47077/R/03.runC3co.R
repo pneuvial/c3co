3382ba9025598bf06e848b53f39117a23b30a11a
Henrik Bengtsson <hb@aroma-project.org>
2019-02-22 14:39:49 +0100

    Using GitHub repos pneuvial/c3co.data (was pneuvial/c3co-data) [#70]

--- inst/GSE47077/R/03.runC3co.R
+++ inst/GSE47077/R/03.runC3co.R
@@ -4,8 +4,8 @@
 ## Run 00.preprocessing.R, 01.CallGenotypes.R, 03.TumorBoost.R before running this file
 library("c3co")
 patient <- "RK29"
-path <- "c3co-data"
-dat <- readRDS(sprintf("%s/dat-%s.rds", path ,patient))
+path <- "c3co.data"
+dat <- readRDS(sprintf("%s/dat-%s.rds", path, patient))
 lambda.grid <- c(2e-6,1e-5,2e-5) ## penalty
 p.list <- 2:13 ## candidate number of subclones
 parameters.grid <- list(lambda1=lambda.grid, lambda2=lambda.grid, nb.arch = p.list)
